32e896a91362b002d93d84a1b2319c604dbc3e1a
IlikeGraphics <146661860+Mey-source@users.noreply.github.com>
2024-10-31 19:57:54 -0700

    Update wCode.cpp
    sigma sigma on the wall whose the skibidiest of them all

--- wCode.cpp
+++ wCode.cpp
@@ -1,5 +1,25 @@
 #include <iostream>
+#include <vector>
+
+//define verts here
+std::vector<int> vert = {
+	0, 0, //vert 1 coords
+	3, 0, //vert 2 coords
+	0, 3, //vert 3 coords
+	3, 0 //vert 4 coords
+};
 
 int main() {
-std::cout << "the creator of this repo is a monkey";
+	for (int i = 0; i < vert.size(); i = i + 2) {
+		for (int a = 0; a < vert[i]; a++) {
+			std::cout << " ";
+		}
+		//borked y axis calc lmao
+		for (int b = 0; b < vert[i + 1]; b++) {
+			std::cout << "\n";
+		}
+		std::cout << "#";
+		std::cout << i;
+	}
+	return 0;
 }
